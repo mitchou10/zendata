9c6bb07264567cbb98742d46701b0933f1ae07ef
michou <misu.helal-ali@interieur.gouv.fr>
2025-05-18 11:19:59 +0200

    add: keypoint

--- src/zendata/data/ml/computer_vision/detection.py
+++ src/zendata/data/ml/computer_vision/detection.py
@@ -2,16 +2,28 @@ from pydantic import Field, BaseModel
 from zendata.data.ml.base import BaseMLData
 
 
-class GroundTruthBbox(BaseModel):
+class Point(BaseModel):
     x: float = Field(..., ge=0.0, le=1.0, description="Relative x position (0<=x <=1)")
     y: float = Field(..., ge=0.0, le=1.0, description="Relative y position (0<=y <=1)")
+    label: str = Field(..., description="Label of the bbox")
+
+
+class PredictPoint(Point):
+    confidence: float = Field(
+        ...,
+        ge=0.0,
+        le=1.0,
+        description="Confidence of the class bbox (0<=Confidence <=1)",
+    )
+
+
+class GroundTruthBbox(Point):
     height: float = Field(
         ..., ge=0.0, le=1.0, description="Relative height position (0<=height <=1)"
     )
     width: float = Field(
         ..., ge=0.0, le=1.0, description="Relative width position (0<=width <=1)"
     )
-    label: str = Field(..., description="Label of the bbox")
 
 
 class PredictBbox(GroundTruthBbox):
@@ -29,7 +41,19 @@ class GtImageDetection(BaseMLData):
     )
 
 
+class GtImageKeypoint(BaseMLData):
+    points: list[Point] = Field(
+        default_factory=list, description="Ground truth key point"
+    )
+
+
 class PredictedImageDetection(BaseMLData):
     bboxes: list[PredictBbox] = Field(
         default_factory=list, description="Predicted bboxes"
+    )
+
+
+class PredictedImageKeypoints(BaseMLData):
+    points: list[PredictPoint] = Field(
+        default_factory=list, description="Predicted keypoint"
     )
