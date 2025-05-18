8732e7feef3419545dea21b364b2bd452b90d4c0
michou <misu.helal-ali@interieur.gouv.fr>
2025-05-18 12:16:03 +0200

    feat: text generation

--- src/zendata/data/ml/base.py
+++ src/zendata/data/ml/base.py
@@ -5,3 +5,5 @@ from zendata.data.base import BaseData
 
 class BaseMLData(BaseData):
     source_id: Optional[str] = Field(None, description="Source id of the data")
+    model_id: Optional[str] = Field(None, description="model id")
+    version: Optional[str] = Field(None, description="Version id of the model")
