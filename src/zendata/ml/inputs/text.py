ff23872c1160a736866bc420e54bdbefdfb0b43a
michou <misu.helal-ali@interieur.gouv.fr>
2025-05-20 07:35:43 +0200

    del: unused data

--- src/zendata/ml/inputs/text.py
+++
@@ -1,8 +0,0 @@
-from typing import Literal
-from .base import BaseInput
-from pydantic import Field
-
-
-class TextInput(BaseInput):
-    type: Literal["text"] = "text"
-    text: str = Field(..., description="Raw text")
